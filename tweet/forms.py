c49db491e00b26fb17d8f74ab7900782c7663d6e
vishalipar <vishalipar04@gmail.com>
2024-07-14 15:44:57 +0530

    first commit

---
+++ tweet/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class Tweetform(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ['text', 'photo']
+        
+    
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
